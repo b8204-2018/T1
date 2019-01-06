a029e15817c8c3cb10b120f9545a9b45b812b514
katebekk <44308249+katebekk@users.noreply.github.com>
2019-01-06 21:52:04 +1000

    print,read,dec
    added libs

--- src/printing_lib.h
+++ src/printing_lib.h
@@ -1,9 +1,16 @@
+#include <iostream>
+#include "printing_lib.h"
 #ifndef T1_PRINTING_LIB_H
 #define T1_PRINTING_LIB_H
 
 /*
  * Выводит в стандартный поток вывода контент
  */
-void printContent(const char *const *const &content);
+void printContent(const char *const *const &content){
+    int count = ((int*)(*content))[1];
+    for (int i = 1; i <= count; i++){
+        printf("%s\n", content[i]);
+    }
+}
 
 #endif //T1_PRINTING_LIB_H
--- src/reading_lib.h
+++ src/reading_lib.h
@@ -1,9 +1,30 @@
+#include <cstring>
+#include <cstdio>
 #ifndef T1_READING_LIB_H
 #define T1_READING_LIB_H
 
 /*
  * Считывает файл, возвращая "массив" строк
  */
-char **readFile(const char *filepath);
+char **readFile(const char *filepath){
+    int len, count;
+    FILE *f = fopen(filepath, "r");
+    if (f == NULL){
+        return nullptr;
+    }
+    fscanf(f, "%d\t%d\n", &len, &count);
+    char **str = new char* [count + 1];
+    int n = sizeof(int)*2;
+    str[0] = new char[n];
+    ((int*) (*str))[0] = len;
+    ((int*) (*str))[1] = count;
+    for (int i = 1; i <= count; i++){
+        str[i] = new char [len];
+        fscanf(f, "%s", str[i]);
+    }
+    fclose(f);
+    return str;
+
+}
 
 #endif //T1_READING_LIB_H
