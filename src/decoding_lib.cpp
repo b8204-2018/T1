e971d1daa9ac96aa01bd9a09bed111733abc99c9
an-kais <kaisarova.angelina@mail.ru>
2019-01-11 21:49:19 +1000

    add decodeStep3

--- src/decoding_lib.cpp
+++ src/decoding_lib.cpp
@@ -26,3 +26,24 @@ void decodeStep1(char **&content){
 }
 
 
+/*
+ * Заменяет каждый 3ий символ в каждой строке на каждый 7ой с конца
+ */
+void decodeStep3(char **&content) {
+
+    int long_ = ((int *) (*content))[0], kol_ = ((int *) (*content))[1];
+    int a = 2;
+    int b = long_ - 6;
+    if (long_ < 8) {
+        cout << " Don't change" << endl;
+    }
+    else {
+        for (int i = 1; i <= kol_; i++) {
+           while (b >= 0)  {
+                swap(content[i][a], content[i][b]);
+                a = a + 3;
+                b = b - 7;
+            }
+        }
+    }
+}
