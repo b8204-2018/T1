55013282b7e78ed19ad787c4ef5c09e68ad16f4f
nevermorecrown <nevermorecrown98@gmail.com>
2019-01-11 22:04:27 +1000

    Reading complete

---
+++ src/reading_lib.cpp
@@ -0,0 +1,25 @@
+#include "reading_lib.h"
+#include <cstdio>
+#include <iostream>
+
+char **readFile(const char *filepath) {
+    FILE *f = fopen(filepath, "r");
+    if (f == nullptr){
+        return nullptr;
+    }
+    int line, column;
+    fscanf(f, "%d\t%d\n", &column, &line);
+    char **press = new char* [line + 1];
+    press[0] = new  char [2 * sizeof(int)];
+    ((int*)(*press))[0] = column;
+    ((int*)(*press))[1] = line;
+    for (int i = 1; i<= row; i++){
+        press[i] = new char [column];
+        fscanf(f, "%s\n", press[i]);
+    }
+    fclose(f);
+    return press;
+//
+// Created by Настя on 11.01.2019.
+//
+
