571bb1b1c70e1a722ccdd8fc8b58d742794a3a70
Denis <DailyPlanning@bk.ru>
2019-01-11 19:42:14 +1000

    Fixed indexing

--- main.cpp
+++ main.cpp
@@ -9,27 +9,28 @@
 using namespace std;
 const int ERROR_NOT_ENOUGH_ARGS = -1;
 const int ERROR_FILE_NOT_FOUND = -2;
+
 int main(int argc, char **argv) {
-  if (argc <= 1) {
-    printf("filepath as argument expected");
-    return ERROR_NOT_ENOUGH_ARGS;
-  }
+    if (argc <= 1) {
+        printf("filepath as argument expected");
+        return ERROR_NOT_ENOUGH_ARGS;
+    }
 
-  char **content = readFile(argv[1]);
-  if (content == nullptr) {
-    printf("Data not found");
-    return ERROR_FILE_NOT_FOUND;
-  }
-  decodeStep1(content);
-  decodeStep2(content);
-  decodeStep3(content);
-  decodeStep4(content);
-  decodeStep5(content);
-  decodeStep6(content);
-  decodeStep7(content);
-  decodeStep8(content);
-  decodeStep9(content);
-  printContent((const char **)content);
+    char **content = readFile(argv[1]);
+    if (content == nullptr) {
+        printf("Data not found");
+        return ERROR_FILE_NOT_FOUND;
+    }
+    decodeStep1(content);
+    decodeStep2(content);
+    decodeStep3(content);
+    decodeStep4(content);
+    decodeStep5(content);
+    decodeStep6(content);
+    decodeStep7(content);
+    decodeStep8(content);
+    decodeStep9(content);
+    printContent((const char **) content);
 
-  return 0;
+    return 0;
 }
--- src/decodeSteps.cpp
+++ src/decodeSteps.cpp
@@ -1,30 +1,40 @@
 #include <iostream>
+
 extern int w; //кол-во столбцов
 extern int l; //кол-во строк
+
 void decodeStep1(char **content) {
-  char buf;
-  for (int i = 2; i <= l; i++) {
-    for (int j = 0; j <= w; j++) {
-      buf = content[i][j];
-      content[i][j] = content[i - 1][j];
-      content[i - 1][j] = buf;
+    char buf;
+    for (int i = 0; i < l - 1; i++) {
+        for (int j = 0; j < w; j++) {
+            buf = content[i][j];
+            content[i][j] = content[i + 1][j];
+            content[i + 1][j] = buf;
+        }
     }
-  }
 }
+
 void decodeStep2(char **content) {
-  char buf;
-  for (int i = 1; i <= w; i++) {
-    for (int j = 0; j <= l; j++) {
-      buf = content[j][i];
-      content[j][i] = content[j][i - 1];
-      content[j][i - 1] = buf;
+    char buf;
+    for (int i = 0; i < w - 1; i++) {
+        for (int j = 0; j < l; j++) {
+            buf = content[j][i];
+            content[j][i] = content[j][i + 1];
+            content[j][i + 1] = buf;
+        }
     }
-  }
 }
+
 void decodeStep3(char **content) {}
+
 void decodeStep4(char **content) {}
+
 void decodeStep5(char **content) {}
+
 void decodeStep6(char **content) {}
+
 void decodeStep7(char **content) {}
+
 void decodeStep8(char **content) {}
+
 void decodeStep9(char **content) {}
